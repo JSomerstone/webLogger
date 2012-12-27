85d05b3a4c7e6e606e80421c5ff76159f88c8d9f
Joona Somerkivi <joona.somerkivi@solinor.com>
2012-12-27 23:13:37 +0200

    Initial commit

---
+++ public/s.gif.php
@@ -0,0 +1,11 @@
+<?php
+date_default_timezone_set('Europe/Helsinki');
+
+$logFile = dirname(__DIR__). '/logs/' . date('Y-m-d') . '.log';
+$logHandle = fopen($logFile, 'w');
+
+fwrite($logHandle, json_encode($_GET));
+
+echo json_encode($_GET);
+//header('Content-Type: image/gif');
+//echo file_get_contents(__DIR__. '/s.gif');
